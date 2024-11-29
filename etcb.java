ac77a641f1166a908cd2142aed2b75d6fbf187e8
JuoG <116748990+Juoelenis@users.noreply.github.com>
2024-11-29 16:34:59 +0200

    Create etcb.java

---
+++ etcb.java
@@ -0,0 +1,38 @@
+package org.dinky.data.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Catalog
+ *
+ * @since 2022/7/17 21:37
+ */
+@Getter
+@Setter
+public class Catalog implements Serializable {
+
+    private static final long serialVersionUID = -7535759384541414568L;
+
+    private String name;
+    private List<Schema> schemas = new ArrayList<>();
+
+    public Catalog() {}
+
+    public Catalog(String name) {
+        this.name = name;
+    }
+
+    public Catalog(String name, List<Schema> schemas) {
+        this.name = name;
+        this.schemas = schemas;
+    }
+
+    public static Catalog build(String name) {
+        return new Catalog(name);
+    }
+}
