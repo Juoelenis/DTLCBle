ace605e8fcd417dc876f2a6b8ca3b2c64330921e
JuoG <116748990+Juoelenis@users.noreply.github.com>
2024-11-29 16:35:39 +0200

    Create AlertINstanceMapper.java

---
+++ aim/AlertINstanceMapper.java
@@ -0,0 +1,14 @@
+package org.dinky.mapper;
+
+import org.dinky.data.model.alert.AlertInstance;
+import org.dinky.mybatis.mapper.SuperMapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * AlertInstanceMapper
+ *
+ * @since 2022/2/24 19:48
+ */
+@Mapper
+public interface AlertInstanceMapper extends SuperMapper<AlertInstance> {}
